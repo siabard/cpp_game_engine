f5ab88cced458cd7679e17fd9f4b9e7fcf3c2247
Yeonho Jang <siabard@gmail.com>
2025-10-15 21:17:16 +0900

    include path 수정

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,38 +1,47 @@
-((c++-mode . ((eval . (setq flycheck-clang-include-path 
+((c++-mode . ((flycheck-clang-language-standard . "c++2a")
+	      (eval . (setq flycheck-clang-include-path 
 			    (list 
-			     (expand-file-name "~/project/cpp/game_engine/include")
-			     (expand-file-name "~/project/cpp/game_engine/vendor/spdlog/include")
-			     "/usr/local/include"
-			     "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/12.0.0/include"
-			     "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include"
-			     "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include"
-			     "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks"
-			     )))
+			     "/usr/local/include/"
+			     "/Library/Developer/CommandLineTools/usr/include/c++/v1"
+			     "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include"
+			     "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/12.0.0/include/"
+			     "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/"
+			     "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/"
+			     "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/"
+			     (expand-file-name "~/project/cpp/game_engine/include/")
+			     (expand-file-name "~/project/cpp/game_engine/vendor/spdlog/include/"))))
 	      (eval . (setq flycheck-gcc-include-path
 			    (list 
-			     (expand-file-name "~/project/cpp/game_engine/include")
-			     (expand-file-name "~/project/cpp/game_engine/vendor/spdlog/include")
-			     "/usr/local/include"
-			     "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/12.0.0/include"
-			     "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include"
-			     "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include"
-			     "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks"))))))
-((c++-ts--mode . ((eval . (setq flycheck-clang-include-path 
+			     "/usr/local/include/"
+			     "/Library/Developer/CommandLineTools/usr/include/c++/v1"
+			     "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include"
+			     "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/12.0.0/include/"
+			     "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/"
+			     "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/"
+			     "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/"
+			     (expand-file-name "~/project/cpp/game_engine/include/")
+			     (expand-file-name "~/project/cpp/game_engine/vendor/spdlog/include/")))))))
+((c++-ts--mode . ((flycheck-clang-language-standard . "c++2a")
+		  (eval . (setq flycheck-clang-include-path 
 				(list 
-				 (expand-file-name "~/project/cpp/game_engine/include")
-				 (expand-file-name "~/project/cpp/game_engine/vendor/spdlog/include")
-				 "/usr/local/include"
-				 "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/12.0.0/include"
-				 "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include"
-				 "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include"
-				 "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks")))
+				 "/usr/local/include/"
+				 "/Library/Developer/CommandLineTools/usr/include/c++/v1"
+				 "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include"
+				 "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/12.0.0/include/"
+				 "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/"
+				 "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/"
+				 "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/"
+				 (expand-file-name "~/project/cpp/game_engine/include/")
+				 (expand-file-name "~/project/cpp/game_engine/vendor/spdlog/include/"))))
 		  (eval . (setq flycheck-gcc-include-path
 				(list 
-				 (expand-file-name "~/project/cpp/game_engine/include")
-				 (expand-file-name "~/project/cpp/game_engine/vendor/spdlog/include")
-				 "/usr/local/include"
-				 "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/12.0.0/include"
-				 "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include"
-				 "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include"
-				 "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks"))))))
+				 "/usr/local/include/"
+				 "/Library/Developer/CommandLineTools/usr/include/c++/v1"
+				 "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include"
+				 "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/12.0.0/include/"
+				 "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/"
+				 "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/"
+				 "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/"
+				 (expand-file-name "~/project/cpp/game_engine/include/")
+				 (expand-file-name "~/project/cpp/game_engine/vendor/spdlog/include/")))))))
 
